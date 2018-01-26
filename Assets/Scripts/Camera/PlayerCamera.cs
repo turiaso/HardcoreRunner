0e934d5b40f5c4d93fe669b53e650b73dbb3c0fb
Antonio J. Narvaez <estopero88@gmail.com>
2018-01-27 00:09:47 +0100

    Working in Camera follow

---
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraFollowPlayer : MonoBehaviour
+{
+
+    public string _tagPlayer;
+
+    [SerializeField]
+    private Transform _player;
+
+    [SerializeField]
+    private float _rotationAngle;
+
+    [SerializeField]
+    private Vector3 _offset;
+    public Vector3 Offset
+    {
+        get
+        {
+            return _offset;
+        }
+        set
+        {
+            _offset = value;
+        }
+    }
+
+    void Start()
+    {
+        GetPlayer();
+    }
+
+    private void GetPlayer()
+    {
+        if (_player == null)
+        {
+            GameObject go = GameObject.FindGameObjectWithTag(_tagPlayer);
+            if (go != null)
+            {
+                _player = go.transform;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (_player == null)
+        {
+            GetPlayer();
+        }
+
+        if (_player == null)
+        {
+            return;
+        }
+
+        SetPosition();
+    }
+
+    private void SetCamera()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.right * _rotationAngle);
+    }
+
+    private void SetPosition()
+    {
+        transform.position = _player.position + _player.forward * Offset.z + _player.up * Offset.y;
+    }
+}

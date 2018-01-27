459ed6ed65a9c8e402eabc8ac4d92fd1a03745f3
Antonio J. Narvaez <estopero88@gmail.com>
2018-01-27 13:00:11 +0100

    some new inputs, working in new fan behavior

---
+++ Assets/Scripts/Fan/FanTotallyRandomBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanTotallyRandomBehavior : MonoBehaviour
+{
+
+    [SerializeField]
+    public float _maxSpeed;
+    [SerializeField]
+    public float _minSpeed;
+
+    public
+}

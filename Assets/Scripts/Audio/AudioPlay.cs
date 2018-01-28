fe69d2031c66faef6703f395042356c0d2bc7be7
Antonio J. Narvaez <estopero88@gmail.com>
2018-01-28 14:45:49 +0100

    fix

---
+++ Assets/Scripts/Audio/AudioPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    public AudioSource _audio;
+
+    public void Play()
+    {
+        _audio.Play();
+    }
+}
